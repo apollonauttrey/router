57e0ae057f3b2e9c22b7914ffb14fbf16baa076e
Matthew Hawkins <matthew.hawkins@apollographql.com>
2024-09-17 21:53:23 -0600

    Validate arrow method names and arity

--- apollo-federation/src/sources/connect/json_selection/parser.rs
+++ apollo-federation/src/sources/connect/json_selection/parser.rs
@@ -263,7 +263,7 @@ impl ExternalVarPaths for NamedSelection {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct PathSelection {
-    pub(super) path: WithRange<PathList>,
+    pub(crate) path: WithRange<PathList>,
 }
 
 // Like NamedSelection, PathSelection is an AST structure that takes its range
@@ -337,7 +337,7 @@ impl From<PathList> for PathSelection {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub(super) enum PathList {
+pub(crate) enum PathList {
     // A VarPath must start with a variable (either $identifier, $, or @),
     // followed by any number of PathStep items (the WithRange<PathList>).
     // Because we represent the @ quasi-variable using PathList::Var, this
@@ -1020,7 +1020,7 @@ pub fn parse_string_literal(input: Span) -> IResult<Span, WithRange<String>> {
 
 #[derive(Debug, PartialEq, Eq, Clone, Default)]
 pub struct MethodArgs {
-    pub(super) args: Vec<WithRange<LitExpr>>,
+    pub(crate) args: Vec<WithRange<LitExpr>>,
     pub(super) range: OffsetRange,
 }
 
